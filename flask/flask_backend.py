0cb0b9d5605235c4267951a8a2e7e441c335ff64
gyanl <gyanlakhwani@gmail.com>
2015-12-29 01:02:19 +0530

    Added POST form, broke things :|

--- flask/flask_backend.py
+++ flask/flask_backend.py
@@ -11,26 +11,29 @@ def check(status_code):
 		else: return 'Taken'
 
 @app.route('/')
-def hi():
-	return "please enter the username in the url"
+def my_form():
+    return render_template("myform.html")
+
+#def hi():
+#	return "Enter your username : "
 
 
-@app.route('/<username>/')
-def user(username):
-	
+#@app.route('/<username>/')
+#def user(username):
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+	username = request.form['text']
 	f = "https://facebook.com/"+username
 	t = "https://twitter.com/"+username
 	i = "https://instagram.com/"+username
 	fr = requests.get(f)
 	tr = requests.get(t)
 	ir = requests.get(i)
-	
 	x = check(fr.status_code)
 	y = check(tr.status_code)
 	z = check(ir.status_code)
-
 	return render_template('world.html', username=username, fr=x, tr=y,ir=z )
-
 
 if __name__ == "__main__":
 	app.run()
