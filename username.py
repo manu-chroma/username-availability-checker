25de8014e6b053de2930853ce9b79834d08b8a61
Manu-chroma <manvendra0310@gmail.com>
2015-12-30 12:51:12 +0530

    Better code structure.

--- username.py
+++ username.py
@@ -5,14 +5,9 @@
 
 import requests
 from termcolor import colored
-username = raw_input("Enter the username : ")
-f = "https://facebook.com/"+username
-t = "https://twitter.com/"+username
-i = "https://instagram.com/"+username
 
-fr = requests.get(f)
-tr = requests.get(t)
-ir = requests.get(i)
+username = raw_input("Enter the username : ")
+sites = ["facebook", "twitter", "instagram", "github"]
 
 def check(status_code):
 	if(status_code == 404):
@@ -21,11 +16,18 @@ def check(status_code):
 		print colored('Taken', 'red')
 
 
-print f
-check(fr.status_code)
+for i, val in enumerate(sites):
+	a = requests.get("https://"+sites[i]+".com/"+username)
+	print "https://"+sites[i]+".com/"+username
+	check(a.status_code)
 
-print t
-check(tr.status_code)
 
-print i
-check(ir.status_code)
+
+
+
+
+
+
+
+
+
