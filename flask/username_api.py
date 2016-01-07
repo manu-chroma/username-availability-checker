4c46a7849259ca3b819e0a75380430227d8df186
Manu-chroma <manvendra0310@gmail.com>
2016-01-08 01:55:06 +0530

    GET instead of HEAD

--- flask/username_api.py
+++ flask/username_api.py
@@ -10,7 +10,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/check/<website>/<username>', methods=['GET'])
 @cross_origin(origin='*')
 def check_username(website,username):
-	return jsonify({'status' : requests.head("http://"+website+"/"+username).status_code})
+	return jsonify({'status' : requests.get("http://"+website+"/"+username).status_code})
 
 if __name__ == '__main__':
     app.run(debug=True)
