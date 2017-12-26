21a91307880359c176799eca827df451ddfadfcc
Manvendra Singh <manvendra0310@gmail.com>
2017-12-26 18:52:34 +0530

    Apply coala patches and fix other linting errors
    This patch is for *.py files. Includes:
    - consistant indentation for all files
    - patches from PEP8Bear, PycodestyleBear
    - PyCharm's inspect code analysis
    - other bears in .coafile
    - optimise imports

--- frontend.py
+++ frontend.py
@@ -1,29 +1,30 @@
 from flask import Flask, request, render_template
-from jinja2 import Template
-import requests
 from flask.ext.cors import CORS, cross_origin
 
-app = Flask(__name__, 
-	static_url_path = '',
-	static_folder = 'static')
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static')
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route('/', methods=['GET'])
 @cross_origin()
 def my_form():
     username = request.args.get('username', '')
     if username:
-        return render_template('status.html', username = username)
-    return render_template("form.html")
+        return render_template('status.html', username=username)
+    return render_template('form.html')
+
 
 @app.route('/', methods=['POST'])
 @cross_origin(origin='*')
 def my_form_post():
-	username = request.form['text']
-	return render_template('status.html', username = username)
+    username = request.form['text']
+    return render_template('status.html', username=username)
 
-if __name__ == "__main__":
-	app.run(host='0.0.0.0',port=8522)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8522)
