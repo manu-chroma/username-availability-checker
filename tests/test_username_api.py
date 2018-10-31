631230a469518166e49af2f4d2fd50f0c435864c
Fransiskus Febryan <franfebryan@gmail.com>
2018-10-31 20:33:01 +0700

    test_username_api.py: Update tests
    This updates the tests to use `username_checker.py` instead of
    `username_api.py`

--- tests/test_username_api.py
+++ tests/test_username_api.py
@@ -7,7 +7,7 @@ import exrex
 import pytest
 import yaml
 
-import username_api
+import username_checker
 
 data = yaml.load(open(os.path.join('tests', 'test_data.yml')))
 websites = yaml.load(open('websites.yml'))
@@ -55,7 +55,7 @@ def generate_random_valid_username(website, length):
     It raise Exception if it cannot generate valid username
     """
     for random_username in generate_random_username(website, length):
-        if username_api.check_format(website, random_username):
+        if username_checker.check_format(website, random_username):
             return random_username
     raise Exception('Cannot generate valid username for username for {} of length {}',
                     website, length)
@@ -68,7 +68,7 @@ def assert_response(app, website, user, status):
 
 
 def get_response(app, website, user):
-    resp_bytes = app.get('/check/{}/{}'.format(website, user))
+    resp_bytes = app.get('/api/check/{}/{}'.format(website, user))
     resp_dict = json.loads(resp_bytes.get_data().decode())
     # this is special to facebook checking,
     # and 'profile' field is only useful for
@@ -84,8 +84,8 @@ def get_expected_response(website, user, status):
         'possible': True,
         'usable': True,
         'status': status,
-        'url': username_api.get_profile_url(website, user),
-        'avatar': username_api.get_avatar(website, user) if status == 200
+        'url': username_checker.get_profile_url(website, user),
+        'avatar': username_checker.get_avatar(website, user) if status == 200
         else None
     }
 
@@ -127,7 +127,7 @@ def custom_name_func(testcase_func, param_num, param):
 class TestUsernameApi(object):
     @classmethod
     def setup_class(self):
-        self.app = username_api.app.test_client()
+        self.app = username_checker.app.test_client()
         self.websites = websites
 
     @parameterized.expand(load_availability_test_cases('available_username'),
@@ -155,9 +155,9 @@ class TestUsernameApi(object):
     @parameterized.expand(load_format_checking_cases(),
                           testcase_func_name=custom_name_func)
     def test_format_checking(self, website, username):
-        resp = self.app.get('/check/{w}/{u}'.format(w=website, u=username))
+        resp = self.app.get('/api/check/{w}/{u}'.format(w=website, u=username))
         json_resp = json.loads(resp.get_data().decode())
-        url = username_api.get_profile_url(website, username)
+        url = username_checker.get_profile_url(website, username)
         assert {
             'possible': False,
             'usable': True,
